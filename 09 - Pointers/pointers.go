599dd0418a900be27c0aa4f12a9fc5e2c3e9e966
NimaAram1 <aramnima50@gmail.com>
2021-05-11 14:08:39 +0430

    learned about address operators and pointers

---
+++ 09 - Pointers/pointers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var a int = 53;
+	var b *int = &a;
+
+	fmt.Print(a,b,"\n")
+	fmt.Println("new: ",*b)
+
+	var j int = 5;
+	k := &j;
+
+	fmt.Printf("%v %p \n",k,k)
+
+	type logo struct{
+		foo int
+	}
+
+	var sm *logo
+	sm = new(logo)
+	sm.foo = 5;
+	fmt.Println(sm.foo)
+
+
+}
